55971fd553fe0578b285b9689af9271537e2972f
eduardoNagashima <eduyudji@gmail.com>
2022-10-28 06:30:52 -0300

    EC2 and RDS connection done, home page done

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import Container from '../components/Container';
-import Navbar from '../components/Navbar';
+import Container from "../components/Container";
+import HomeContent from "../components/HomeContent";
+import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <Container>
-      <Navbar select={'Home'}/>
+      <Navbar select={"Home"} />
+      <HomeContent />
     </Container>
   );
 }
